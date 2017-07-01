f284c970c1e621a68a140c3c22a63b30e1af6f8c
Edwin Moses <workockmoses@gmail.com>
2017-07-01 17:13:25 +0300

    toolTip initialized

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ var height = 700;
 var svg = d3.select(".container").append("svg")
   .attr("width", width)
   .attr("height", height)
+
+var toolTip = d3.select(".container").append("div").attr("class", "toolTip");
+
+d3.json("/data.json", (d) => {
+
+});
